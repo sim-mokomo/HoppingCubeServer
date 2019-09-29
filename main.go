39d3b5eebabad74adbe463d512b4ae969363acd1
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-09-30 00:53:32 +0900

    【ADD】POSTリクエストのjsonを受け取ってそのまま返す処理実装

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 )
+
+type StageConfig struct {
+	Name        string
+	PanelLayout string
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello,World\n")
@@ -19,7 +26,34 @@ func testEntryCreatedStage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if method == "POST" {
-		fmt.Fprint(w, "test post request response")
+
+		if r.Header.Get("Content-Type") != "application/json" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		requestLength, err := strconv.Atoi(r.Header.Get("Content-Length"))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		body := make([]byte, requestLength)
+		requestLength, err = r.Body.Read(body)
+		if err != nil && err != io.EOF {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		var jsonBody map[string]interface{}
+		err = json.Unmarshal(body[:requestLength], &jsonBody)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprint(w, jsonBody)
+		w.WriteHeader(http.StatusOK)
 		return
 	}
 }

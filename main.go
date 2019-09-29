9cd256fde9e5ffd7124ab9eddc3cfd2d20ca4e48
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-09-29 20:00:56 +0900

    【ADD】レスポンステスト用のAPIを作成した

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -10,8 +11,22 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello,World\n")
 }
 
+func testEntryCreatedStage(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+	if method == "GET" {
+		fmt.Fprint(w, "test get request response")
+		return
+	}
+
+	if method == "POST" {
+		fmt.Fprint(w, "test post request response")
+		return
+	}
+}
+
 func main() {
 	port := os.Getenv("PORT")
+	http.HandleFunc("/create_stage", testEntryCreatedStage)
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":"+port, nil)
 }
